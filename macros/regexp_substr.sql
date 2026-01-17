23fa91e9c5eca8a5a2e8c25c068ff91c129aac7a
Mike Krahulec <mike@z2healthinsights.com>
2026-01-17 13:54:02 -0600

    Fix macro for regexp_substr

--- macros/regexp_substr.sql
+++ macros/regexp_substr.sql
@@ -6,38 +6,38 @@
 
 #}db
 
-{%- macro regexp_substr(string, pattern, position=1, occurrence=1, match_type='') -%}
+{%- macro regexp_substr(string, pattern, position=1, occurrence=1) -%}
 
-    {{ return(adapter.dispatch('regexp_substr')(string, pattern, position, occurrence, match_type)) }}
+    {{ return(adapter.dispatch('regexp_substr')(string, pattern, position, occurrence)) }}
 
 {%- endmacro -%}
 
-{%- macro duckdb__regexp_substr(string, pattern, position, occurrence, match_type) -%}
+{%- macro duckdb__regexp_substr(string, pattern, position, occurrence) -%}
 
     regexp_extract( {{ string }}, {{ pattern }} )
 
 {%- endmacro -%}
 
-{%- macro default__regexp_substr(string, pattern, position, occurrence, match_type) -%}
-
-    regexp_substr( {{ string }}, {{ pattern }}, {{ position }}, {{ occurrence }}, {{ match_type }} )
-
-{%- endmacro -%}
-
-{%- macro bigquery__regexp_substr(string, pattern, position, occurrence, match_type) -%}
-
-    regexp_extract_all( {{ string }}, {{ pattern }} )[safe_offset(0)]
-
-{%- endmacro -%}
-
-{%- macro redshift__regexp_substr(string, pattern, position, occurrence, match_type) -%}
+{%- macro default__regexp_substr(string, pattern, position, occurrence) -%}
 
     regexp_substr( {{ string }}, {{ pattern }}, {{ position }}, {{ occurrence }} )
 
 {%- endmacro -%}
 
-{%- macro snowflake__regexp_substr(string, pattern, position, occurrence, match_type) -%}
+{%- macro bigquery__regexp_substr(string, pattern, position, occurrence) -%}
 
-    regexp_substr( {{ string }}, {{ pattern }}, {{ position }}, {{ occurrence }}, {{ match_type }} )
+    regexp_extract_all( {{ string }}, {{ pattern }} )[safe_offset(0)]
+
+{%- endmacro -%}
+
+{%- macro redshift__regexp_substr(string, pattern, position, occurrence) -%}
+
+    regexp_substr( {{ string }}, {{ pattern }}, {{ position }}, {{ occurrence }} )
+
+{%- endmacro -%}
+
+{%- macro snowflake__regexp_substr(string, pattern, position, occurrence) -%}
+
+    regexp_substr( {{ string }}, {{ pattern }}, {{ position }}, {{ occurrence }} )
 
 {%- endmacro -%}
